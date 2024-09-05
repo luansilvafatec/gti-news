27cc428b6070303abdcd10a458d2dd45fff0ffe1
luansilvafatec <luan.silva99@fatec.sp.gov.br>
2024-09-05 10:38:02 -0300

    criacao do projeto, tailwind, e tabelas

---
+++ resources/views/tela-teste.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Tela de teste</title>
+</head>
+<body>
+
+<img src="hjkhkhkj.jpg" alt="">
+<a href="http://"></a>
+
+
+
+<h1 class="bg-black p-4 rounded-lg text-white">Hello Mundo teste</h1>
+    
+</body>
+</html>
