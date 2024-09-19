fe1587258101ff8957883a35e35f72508a00322d
luansilvafatec <luan.silva99@fatec.sp.gov.br>
2024-09-19 11:00:44 -0300

    comando incial, base layout e salvar usuario

--- resources/views/tela-cadastro.blade.php
+++ resources/views/tela-cadastro.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>GTI News - Cadastre-se!</title>
-</head>
-
-<body>
+<x-base>
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
             <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Crie sua conta</h2>
-            <form action="#">
+            <form action="{{route('SalvaUsuario')}}" method="post">
+                @csrf
                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                     <div class="sm:col-span-2">
                         <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
@@ -38,6 +29,4 @@
             </form>
         </div>
     </section>
-</body>
-
-</html>
+</x-base>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +11,23 @@ Route::get('/', function () {
 Route::view('/teste', 'tela-teste');
 
 Route::view('/cadastro', 'tela-cadastro');
+
+Route::post('/salva-usuario', 
+    function (Request $request){
+        $user = new User();
+        $user->name = $request->nome;
+        $user->email = $request->email;
+        $user->password = $request->senha;
+        $user->save();
+
+        return "Salvo com sucesso";
+    }
+)->name('SalvaUsuario');
+
+
+
+
+
+
+
+
